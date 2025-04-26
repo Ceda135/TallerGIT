a293378c87773427b4b6d8bd8acdaddaa176dff4
Ceda135 <cdherrera2005@gmail.com>
2025-04-25 22:14:52 -0500

    Estudiante

---
+++ src/tallergit/Estudiante.java
@@ -0,0 +1,15 @@
+package tallergit;
+
+public class Estudiante {
+    private String nombre;
+    private int edad;
+
+    public Estudiante(String nombre, int edad) {
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+
+    public void saludar() {
+        System.out.println("Hola, soy " + nombre + " y tengo " + edad + " años.");
+    }
+}
